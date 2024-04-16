302a702d3246511ebbdcf2f823fa0edd5824c544
Deepika-Prasad <prasad.deepika91@gmail.com>
2024-04-16 21:46:25 +0530

    initial commit!

---
+++ src/main/java/com/depika/brave/BraveApplication.java
@@ -0,0 +1,13 @@
+package com.depika.brave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraveApplication.class, args);
+	}
+
+}
